2f38fb57ba42f785d328ca203acaa1d97130b161
panand <panand@fas.harvard.edu>
2017-11-21 16:18:01 -0500

    Packaging

--- affinity_binder/run_2013.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=1 python train_rnacompete_2013.py
-CUDA_VISIBLE_DEVICES=1 python validation_rnacompete_2013.py
-CUDA_VISIBLE_DEVICES=1 python test_rnacompete_2013.py
-
-CUDA_VISIBLE_DEVICES=1 python plot_rnacompete_2013_saliency.py
-CUDA_VISIBLE_DEVICES=1 python plot_rnacompete_2013_motifs.py
-CUDA_VISIBLE_DEVICES=1 python plot_rnacompete_2013_ensemble_motifs.py
